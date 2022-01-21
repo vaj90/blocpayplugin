2f1d142ff15dad5598daea72f3d0a4a2da0c4ad2
Allan John <allanjohn.valiente@gmail.com>
2022-01-21 12:49:15 -0500

    updated widget, some features had been added

--- blocpay.js
+++ blocpay.js
@@ -1,11 +1,27 @@
 (function($){
+	var blocPayPop = (opt) => {
+        const newWindow = window.open(opt.url, opt.title, 
+            `
+            scrollbars=yes,
+            width=${opt.width}, 
+            height=${opt.height}, 
+            top=${opt.top}, 
+            left=${opt.left}
+            `
+        )
+        if (window.focus) newWindow.focus();
+    }
+	var ext =["jpeg","png","gif","bmp","svg"];
 	var _defaults = {
         isModal: false,
         url: "",
         btn: {
-            "class" : "btn",
-            "icon": "",
-            "text": "blocpay"
+            "class" : "blocpay-plain",
+            "icon": "assets/img/icon.png",
+            "text": "BlocPay",
+			"event": blocPayPop,
+			"customContent": "",
+			"customEvent": null
         },
         title: "Bloc Pay",
         width: 640,
@@ -20,31 +36,48 @@
 		this.init();
 	}
 
-    var blocPayPop = (opt) => {
-        const newWindow = window.open(opt.url, opt.title, 
-            `
-            scrollbars=yes,
-            width=${opt.width}, 
-            height=${opt.height}, 
-            top=${opt.top}, 
-            left=${opt.left}
-            `
-        )
-        if (window.focus) newWindow.focus();
-    }
 	BlocPay.fn = BlocPay.prototype = {
 		init: function(){
 			var elem = this._element;
 			var opt = this._options;
-            opt.left = (screen.width - opt.width) / 2,
+            opt.left = (screen.width - opt.width) / 2
             opt.top = (screen.height - opt.height) / 4
-			elem.append(
-				$("<button>").attr({
-					"class": `blocpay-btn ${_defaults.btn.class}`
-				}).html(_defaults.btn.text).click(function(){
-					blocPayPop(opt);
+			var $btn = $("<button>");
+			if(opt.btn.class!=""){
+				$btn.attr({"class": `blocpay ${opt.btn.class}`})
+			}
+			if(opt.btn.icon!=""){
+				if(ext.indexOf(opt.btn.icon)){
+					$btn.append($("<img>").attr({"src": opt.btn.icon}))
+				}
+			}			
+			if(opt.btn.text!=""){
+				$btn.append($("<span>").html(opt.btn.text))
+			}
+			if(opt.btn.customContent!=""){
+				$btn.html(opt.btn.custom)
+			}
+			if(opt.btn.event!=null){
+				$btn.click(function(){
+					if(!opt.isModal){
+						opt.btn.event(opt)
+					}
+					else{
+						var dialog = bootbox.dialog({
+							message: `<div class='blocpay-container'>
+										<iframe src='${opt.url}' class="border-0"></iframe>
+									</div>`,
+							size: 'md'
+						});
+					}
 				})
-			)
+			}
+			if(opt.btn.customEvent!=null){
+				$btn.click(function(){
+					opt.btn.customEvent
+				})
+			}
+			elem.append($btn)
 		},
 		isModal: function(){
 			var args = arguments;
@@ -59,6 +92,38 @@
 			if(args.length == 0) return this._options.url;
 			else if(args.length > 0 && typeof args[0] === "string"){
 				this._options.url = args[0];
+				return this;
+			}
+		},	
+		title: function(){
+			var args = arguments;
+			if(args.length == 0) return this._options.title;
+			else if(args.length > 0 && typeof args[0] === "string"){
+				this._options.title = args[0];
+				return this;
+			}
+		},	
+		width: function(){
+			var args = arguments;
+			if(args.length == 0) return this._options.width;
+			else if(args.length > 0 && typeof args[0] === "number"){
+				this._options.width = args[0];
+				return this;
+			}
+		},	
+		height: function(){
+			var args = arguments;
+			if(args.length == 0) return this._options.height;
+			else if(args.length > 0 && typeof args[0] === "number"){
+				this._options.height = args[0];
+				return this;
+			}
+		},	
+		btn: function(){
+			var args = arguments;
+			if(args.length == 0) return this._options.btn;
+			else if(args.length > 0 && typeof args[0] === "object"){
+				this._options.btn = args[0];
 				return this;
 			}
 		},	
